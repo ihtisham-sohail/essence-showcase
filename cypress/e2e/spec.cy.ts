6604e5d390a6afeb557cc7eacd35df38a55c45dd
Ihtisham Sohail <ihtisham.sohail.b@gmail.com>
2023-10-10 13:17:44 +0100

    added images + cypress setup

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,10 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Welcome to Social Centralize');
+    cy.get('#google-sso')
+      .should('have.length', 1)
+      .and('have.class', 'round-btn')
+      .click();
+  });
+});
